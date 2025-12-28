8c1531b5c9d747a669dff66e5cd13b371cac0c91
Bryenne[Sylph] <mrrudix@gmail.com>
2025-12-28 01:44:12 -0500

    Add files via upload
    Database of all possible home points you can warp to.

---
+++ homepoints.lua
@@ -0,0 +1,132 @@
+--[[
+Copyright © 2025, Bryenne [Sylph] - DragonGuard linkshell
+
+THIS DATA IS PROVIDED FOR USE OF THE "Tourist Add-On for Windower" ONLY. IF YOU USE THIS DATE FOR OTHER PURPOSES, PLEASE CREDIT WHERE CREDIT IS DUE!
+
+	* Do not use for commercial products, Add-On bundles sold for profit or any other product endorsements without written permission. My lawyer will find you kupo. 
+	
+Shout-Outs: Rubenator, Dimitrious, AkadenTK
+]]
+return {
+	[0] = {id=0, zone=230}, -- Southern San d'Oria
+	[1] = {id=1, zone=230},
+	[2] = {id=2, zone=230},
+	[97] = {id=97, zone=230},
+	[3] = {id=3, zone=231}, -- Northern San d'Oria
+	[4] = {id=4, zone=231},
+	[5] = {id=5, zone=231},
+	[98] = {id=98, zone=231},
+	[6] = {id=6, zone=232}, -- Port San d'Oria
+	[7] = {id=7, zone=232},
+	[8] = {id=8, zone=232},
+	[9] = {id=9, zone=234}, -- Bastok Mines
+	[10] = {id=10, zone=234},
+	[99] = {id=99, zone=234},
+	[11] = {id=11, zone=235}, -- Bastok Markets
+	[12] = {id=12, zone=235},
+	[13] = {id=13, zone=235},
+	[100] = {id=100, zone=235},
+	[14] = {id=14, zone=236}, -- Port Bastok
+	[15] = {id=15, zone=236},
+	[101] = {id=101, zone=236},
+	[16] = {id=16, zone=237}, -- Metalworks
+	[102] = {id=102, zone=237},
+	[17] = {id=17, zone=238}, -- Windurst Waters
+	[18] = {id=18, zone=238},
+	[103] = {id=103, zone=238},
+	[118] = {id=118, zone=238},
+	[19] = {id=19, zone=239}, -- Windurst Walls
+	[20] = {id=20, zone=239},
+	[21] = {id=21, zone=239},
+	[22] = {id=22, zone=240}, -- Port Windurst
+	[23] = {id=23, zone=240},
+	[24] = {id=24, zone=240},
+	[25] = {id=25, zone=241}, -- Windurst Woods
+	[26] = {id=26, zone=241},
+	[27] = {id=27, zone=241},
+	[28] = {id=28, zone=241},
+	[119] = {id=119, zone=241},
+	[29] = {id=29, zone=243}, -- Ru'Lude Gardens
+	[30] = {id=30, zone=243},
+	[31] = {id=31, zone=243},
+	[32] = {id=32, zone=244}, -- Upper Jeuno
+	[33] = {id=33, zone=244},
+	[34] = {id=34, zone=244},
+	[35] = {id=35, zone=245}, -- Lower Jeuno
+	[36] = {id=36, zone=245},
+	[37] = {id=37, zone=246}, -- Port Jeuno
+	[38] = {id=38, zone=246},
+	[39] = {id=39, zone=250}, -- Kazham
+	[40] = {id=40, zone=249}, -- Mhaura
+	[41] = {id=41, zone=252}, -- Norg
+	[104] = {id=104, zone=252},
+	[42] = {id=42, zone=247}, -- Raboa
+	[105] = {id=105, zone=247},
+	[43] = {id=43, zone=248}, -- Selbina
+	[44] = {id=44, zone=256}, -- Western Adoulin
+	[109] = {id=109, zone=256},
+	[45] = {id=45, zone=257}, -- Eastern Adoulin
+	[110] = {id=110, zone=257},
+	[46] = {id=46, zone=261}, -- Ceizak Battlegrounds
+	[47] = {id=47, zone=262}, -- Foret de Hennetiel
+	[48] = {id=48, zone=265}, -- Morimar Basalt Fields
+	[49] = {id=49, zone=263}, -- Yorcia Weald
+	[50] = {id=50, zone=266}, -- Marjami Ravine
+	[51] = {id=51, zone=267}, -- Kamihr Drifts
+	[52] = {id=52, zone=142}, -- Yughott Grotto
+	[53] = {id=53, zone=143}, -- Palborough Mines
+	[54] = {id=54, zone=145}, -- Giddeus
+	[55] = {id=55, zone=204}, -- Fei'Yin
+	[94] = {id=94, zone=204},
+	[56] = {id=56, zone=208}, -- Quicksand Caves
+	[96] = {id=96, zone=208},
+	[57] = {id=57, zone=160}, -- Den of Rancor
+	[93] = {id=93, zone=160},
+	[58] = {id=58, zone=162}, -- Castle Zvahl Keep
+	[59] = {id=59, zone=130}, -- Ru'Aun Gardens
+	[60] = {id=60, zone=130},
+	[61] = {id=61, zone=130},
+	[62] = {id=62, zone=130},
+	[63] = {id=63, zone=130},
+	[64] = {id=64, zone=26}, -- Tavnazian Safehold
+	[120] = {id=120, zone=26},
+	[121] = {id=121, zone=26},
+	[65] = {id=65, zone=50}, -- Whitegate
+	[106] = {id=106, zone=50},
+	[107] = {id=107, zone=50},
+	[108] = {id=108, zone=50},
+	[66] = {id=66, zone=53}, -- Nashmau
+	[68] = {id=68, zone=80}, -- Southern San d'Oria [S]
+	[69] = {id=69, zone=87}, -- Bastok Markets [S]
+	[70] = {id=70, zone=94}, -- Windurst Waters [S]
+	[71] = {id=71, zone=158}, -- Upper Delkfutt's Tower
+	[72] = {id=72, zone=178}, -- The Shrine of Ru'Avitau
+	[73] = {id=73, zone=29}, -- Riverne Site #B01
+	[74] = {id=74, zone=52}, -- Bhaflau Thickets
+	[75] = {id=75, zone=79}, -- Caedarva Mire
+	[76] = {id=76, zone=5}, -- Uleguerand Range
+	[77] = {id=77, zone=5},
+	[78] = {id=78, zone=5},
+	[79] = {id=79, zone=5},
+	[80] = {id=80, zone=5},
+	[81] = {id=81, zone=7}, -- Attohwa Chasm
+	[82] = {id=82, zone=9}, -- Pso'Xja
+	[83] = {id=83, zone=12}, -- Newton
+	[84] = {id=84, zone=30}, -- Riverne Site #A01
+	[85] = {id=85, zone=33}, -- Al'Taieu
+	[86] = {id=86, zone=33},
+	[87] = {id=87, zone=33},
+	[88] = {id=88, zone=34}, -- Grand Palace of hooizooi
+	[89] = {id=89, zone=35}, -- Ru'Hmet
+	[90] = {id=90, zone=61}, -- Mt. Zhayolm
+	[91] = {id=91, zone=113}, -- Cape Terrigan
+	[92] = {id=92, zone=153}, -- The Boyahda Tree
+	[95] = {id=95, zone=205}, -- Ifrit's Cauldron
+	[111] = {id=111, zone=137}, -- Xarcabard [S]
+	[112] = {id=112, zone=281}, -- Leafallia
+	[113] = {id=113, zone=155}, -- Castle Zvahl Keep [S]
+	[114] = {id=114, zone=126}, -- Qufim
+	[115] = {id=115, zone=169}, -- Toraimorai Canal
+	[116] = {id=116, zone=276}, -- Ra'Kaznar Inner Court
+	[117] = {id=117, zone=25}, -- Misareaux Coast
+}, {"id", "zone"}
