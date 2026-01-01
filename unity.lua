c5a1a94a710574d2cc4c32b761c17be22ada72d6
Bryenne[Sylph] <mrrudix@gmail.com>
2026-01-01 14:19:11 -0500

    Add files via upload
    Table for warping through Unity NPCs

---
+++ unity.lua
@@ -0,0 +1,65 @@
+--[[
+Copyright © 2025{zone=}, Bryenne [Sylph] - DragonGuard linkshell
+
+THIS DATA IS PROVIDED FOR USE OF THE "TrustCards Add-On for Windower" ONLY. IF YOU USE THIS DATE FOR OTHER PURPOSES{zone=}, PLEASE CREDIT WHERE CREDIT IS DUE!
+
+	* Do not use for commercial products{zone=}, Add-On bundles sold for profit or any other product endorsements without written permission. My lawyer will find you kupo. 
+	
+Shout-Outs: Dimitrious{zone=}, Kronik{zone=}, Azykros
+]]
+return {
+    [1] = {zone=101}, -- East Ronfaure
+	[33] = {zone=107}, -- South Gustaberg
+	[65] = {zone=116}, -- East Sarutabaruta
+	[97] = {zone=102}, -- La Theine Plateau
+	[129] = {zone=108}, -- Konschtat Highlands
+	[161] = {zone=117}, -- Tahrongi Canyon
+	[193] = {zone=103}, -- Valkurm Dunes
+	[225] = {zone=118}, -- Buburimu Peninsula
+	[257] = {zone=126}, -- Qufim Island
+	[289] = {zone=4}, -- Bibiki Bay
+	[321] = {zone=2}, -- Carpenters Landing
+	[353] = {zone=123}, -- Yuhtunga Jungle
+	[385] = {zone=24}, -- Lufaise Meadows
+	[417] = {zone=104}, -- Jugner Forest
+	[449] = {zone=109}, -- Pashhow Marshlands
+	[481] = {zone=119}, -- Meriphataud Mountains
+	[513] = {zone=114}, -- Eastern Altepa Desert
+	[545] = {zone=124}, -- Yhoator Jungle
+	[577] = {zone=121}, -- The Sanctuary of Zitah
+	[609] = {zone=25}, -- Misareaux Coast
+	[641] = {zone=213}, -- Labyrinth of Onzozo
+	[673] = {zone=167}, -- Bostaunieux
+	[705] = {zone=105}, -- Batallia Downs
+	[737] = {zone=110}, -- Rolanberry Fields
+	[769] = {zone=120}, -- Sauromogue Champaign
+	[801] = {zone=111}, -- Beaucedine Glacier
+	[833] = {zone=112}, -- Xarcabard
+	[865] = {zone=122}, -- Ro'Maeve
+	[897] = {zone=125}, -- Western Altepa Desert
+	[929] = {zone=7}, -- Attohwa Chasm
+	[961] = {zone=200}, -- Garlaige Citadel
+	[993] = {zone=205}, -- Ifrit's Cauldron
+	[1025] = {zone=153}, -- The Boyahda Tree
+	[1057] = {zone=174}, -- Kuftal Tunnel
+	[1089] = {zone=176}, -- Sea Serpent Grotto
+	[1121] = {zone=159}, -- Temple of Uggalepih
+	[1153] = {zone=208}, -- Quicksand Caves
+	[1185] = {zone=51}, -- Wajoam Woodlands
+	[1217] = {zone=24}, -- Lufaise Meadows
+	[1249] = {zone=113}, -- Cape Teriggan
+	[1313] = {zone=5}, -- Uleguerand Range
+	[1345] = {zone=160}, -- Den of Rancor
+	[1377] = {zone=204}, -- Fei'Yin
+	[1441] = {zone=72}, -- Alzadaal Undersea Ruins
+	[1473] = {zone=25}, -- Misareaux Coast
+	[1505] = {zone=61}, -- Mounth Zhayolm
+	[1537] = {zone=212}, -- Gustav Tunnel
+	[1569] = {zone=127}, -- behemoth's Dominion
+	[1601] = {zone=153}, -- The Boyahda Tree
+	[1633] = {zone=128}, -- Valley of Sorrows
+	[1665] = {zone=51}, -- Wajoam Woodlands
+	[1697] = {zone=61}, -- Mounth Zhayolm
+	[1729] = {zone=79}, -- Caedarva Mire
+	[1761] = {zone=68} -- Aydeewa Subterrane
+}
