46b7ad32c96de7ad084e9c2fecc3011ef9b683c2
Bryenne[Sylph] <mrrudix@gmail.com>
2026-01-01 16:03:40 -0500

    Add files via upload

---
+++ waypoints.lua
@@ -0,0 +1,79 @@
+--[[
+Copyright © 2025, Bryenne [Sylph] - DragonGuard linkshell
+
+THIS DATA IS PROVIDED FOR USE OF THE "Tourist Add-On for Windower" ONLY. IF YOU USE THIS DATE FOR OTHER PURPOSES, PLEASE CREDIT WHERE CREDIT IS DUE!
+
+	* Do not use for commercial products, Add-On bundles sold for profit or any other product endorsements without written permission. My lawyer will find you kupo. 
+	
+Shout-Outs: Rubenator, Dimitrious, AkadenTK
+]]
+
+return {
+	[1] = {id=1, zone=256}, -- Western Adoulin
+	[2] = {id=2, zone=256},
+	[3] = {id=3, zone=256},
+	[4] = {id=4, zone=256},
+	[5] = {id=5, zone=256},
+	[6] = {id=6, zone=256},
+	[7] = {id=7, zone=256},
+	[8] = {id=8, zone=256},
+	[9] = {id=9, zone=256},
+	[21] = {id=21, zone=257}, -- Eastern Adoulin
+	[22] = {id=22, zone=257},
+	[23] = {id=23, zone=257},
+	[24] = {id=24, zone=257},
+	[25] = {id=25, zone=257},
+	[26] = {id=26, zone=257},
+	[27] = {id=27, zone=257},
+	[28] = {id=28, zone=257},
+	[29] = {id=29, zone=257},
+	[31] = {id=31, zone=260}, -- Yahse Hunting Grounds
+	[32] = {id=32, zone=260},
+	[33] = {id=33, zone=260},
+	[34] = {id=34, zone=260},
+	[41] = {id=41, zone=261}, -- Ceizak Battlegrounds
+	[42] = {id=42, zone=261},
+	[43] = {id=43, zone=261},
+	[44] = {id=44, zone=261},
+	[51] = {id=51, zone=262}, -- Foret de Hennetiel
+	[52] = {id=52, zone=262},
+	[53] = {id=53, zone=262},
+	[54] = {id=54, zone=262},
+	[55] = {id=55, zone=262},
+	[61] = {id=61, zone=265}, -- Morimar Basalt Fields
+	[62] = {id=62, zone=265},
+	[63] = {id=63, zone=265},
+	[64] = {id=64, zone=265},
+	[65] = {id=65, zone=265},
+	[65] = {id=65, zone=265},
+	[71] = {id=71, zone=263}, -- Yorcia Weald
+	[72] = {id=72, zone=263},
+	[73] = {id=73, zone=263},
+	[74] = {id=74, zone=263},
+	[302] = {id=302, zone=263}, -- (enigmatic device)
+	[81] = {id=81, zone=266}, -- Marjami Ravine
+	[82] = {id=82, zone=266},
+	[83] = {id=83, zone=266},
+	[84] = {id=84, zone=266},
+	[85] = {id=85, zone=266},
+	[91] = {id=91, zone=267}, -- Kamihr Drift
+	[92] = {id=92, zone=267},
+	[93] = {id=93, zone=267},
+	[94] = {id=94, zone=267},
+	[95] = {id=95, zone=267},
+	[100] = {id=100, zone=245}, -- Lower Jeuno
+	[300] = {id=300, zone=258}, -- Rala Waterways Enigmatic Device
+	[301] = {id=301, zone=270}, -- Cirdas Caverns Enigmatic Device
+	[303] = {id=303, zone=274}, -- Outer Ra'Kazar Enigmatic Device
+	[200] = {id=200, zone=231}, -- Northern San d'Oria
+	[201] = {id=201, zone=235}, -- Bastok Markets
+	[202] = {id=202, zone=241}, -- Windurst Woods
+	[203] = {id=203, zone=248}, -- Selbina
+	[204] = {id=204, zone=249}, -- Mhaura
+	[205] = {id=205, zone=250}, -- Kazham
+	[206] = {id=206, zone=247}, -- Raboa
+	[207] = {id=207, zone=252}, -- Norg
+	[208] = {id=208, zone=26}, -- Tavnazian Safehold
+	[209] = {id=209, zone=50}, -- Whitegate
+	[210] = {id=210, zone=53}, -- Nashmau
+}, {"id", "zone"}
